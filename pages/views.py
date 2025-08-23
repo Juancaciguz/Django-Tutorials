a9f5e5e5ca5a549986b51c137b0a6748ac9072d5
Juan Carlos Citelly Guzmán <juancaciguz@hotmail.com>
2025-08-23 14:07:16 -0500

    Activity 2 done

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView 
+from django.views import View 
 # Create your views here.
 class HomePageView(TemplateView): 
     template_name = 'pages/home.html'
@@ -27,3 +28,37 @@ class ContactPageView(TemplateView):
             "phone": "+1234567890",
         })
         return context
+    
+
+ 
+class Product: 
+    products = [ 
+        {"id":"1", "name":"TV", "description":"Best TV"}, 
+        {"id":"2", "name":"iPhone", "description":"Best iPhone"}, 
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"}, 
+        {"id":"4", "name":"Glasses", "description":"Best Glasses"} 
+    ] 
+ 
+class ProductIndexView(View): 
+    template_name = 'products/index.html' 
+ 
+    def get(self, request): 
+        viewData = {} 
+        viewData["title"] = "Products - Online Store" 
+        viewData["subtitle"] =  "List of products" 
+        viewData["products"] = Product.products 
+ 
+        return render(request, self.template_name, viewData) 
+ 
+class ProductShowView(View): 
+    template_name = 'products/show.html' 
+ 
+ 
+    def get(self, request, id): 
+        viewData = {} 
+        product = Product.products[int(id)-1] 
+        viewData["title"] = product["name"] + " - Online Store" 
+        viewData["subtitle"] =  product["name"] + " - Product information" 
+        viewData["product"] = product 
+ 
+        return render(request, self.template_name, viewData)
