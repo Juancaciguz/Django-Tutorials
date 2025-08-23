8923a20898d115be4f65b0114e6d98e7fcee2da9
Juan Carlos Citelly Guzmán <juancaciguz@hotmail.com>
2025-08-23 14:11:00 -0500

    Activity 3 done

--- pages/views.py
+++ pages/views.py
@@ -33,10 +33,10 @@ class ContactPageView(TemplateView):
  
 class Product: 
     products = [ 
-        {"id":"1", "name":"TV", "description":"Best TV"}, 
-        {"id":"2", "name":"iPhone", "description":"Best iPhone"}, 
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"}, 
-        {"id":"4", "name":"Glasses", "description":"Best Glasses"} 
+        {"id":"1", "name":"TV", "description":"Best TV", "price": 800.00}, 
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price": 700.00}, 
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price": 50.00}, 
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price": 300.00}, 
     ] 
  
 class ProductIndexView(View): 
